ad5a077b48854e5f5ae4cb169f3be113e0829d0b
TAEWOONG KANG <77313674+Taewoong1378@users.noreply.github.com>
2021-12-19 20:49:56 +0900

    Change js file to ts file & delete prop-types module
    * Change js file to ts file & delete prop-types module
    
    * test page 오류 발견돼 revert한 후 수정

--- front/src/pages/test.tsx
+++ front/src/pages/test.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:front/src/pages/test.tsx
-import { Test } from '@containers';
-=======
 import { Test } from '@modules';
->>>>>>> origin/develop:front/src/pages/test.js
 import React from 'react';
 
 const TestPage = () => <Test />;
