6a7c8909ef5c0f965bdcde3d26cf1ccfa39775f1
jm <rjm9303@gmail.com>
2021-12-21 00:51:52 +0900

    Migrate to Kotlin

---
+++ back/src/main/java/com/simplediary/back/config/JpaAuditingConfig.kt
@@ -0,0 +1,8 @@
+package com.simplediary.back.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfig 
