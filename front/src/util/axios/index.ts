064c0eed525acff896ec5b030f7729deba3327f9
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-01-02 15:32:38 +0900

    axois 관련 타입 설정

---
+++ front/src/util/axios/index.ts
@@ -0,0 +1,9 @@
+export * from './customAxios';
+export {
+  SuccessStatus,
+  RedirectStatus,
+  ClientErrorStatus,
+  ServerErrorStatus,
+  ErrorStatus,
+} from './customAxios.types';
+export type { ApiError } from './customAxios.types';
