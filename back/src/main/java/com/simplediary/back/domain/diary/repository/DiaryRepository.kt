21450374fdd28bf7d15e767e13c1f9df475faf7b
jm <rjm9303@gmail.com>
2022-01-02 23:16:07 +0900

    package conflicts resolved

--- back/src/main/java/com/simplediary/back/domain/diary/repository/DiaryRepository.kt
+++ back/src/main/java/com/simplediary/back/domain/diary/repository/DiaryRepository.kt
@@ -3,7 +3,9 @@ package com.simplediary.back.domain.diary.repository
 import com.simplediary.back.domain.diary.entity.Diary
 import com.simplediary.back.domain.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface DiaryRepository : JpaRepository<Diary?, Long?> {
   fun findByUser(user: User): Diary?
 }
